224c9d1b839c6c2ed433b665864b5f1646a171a5
FedeCF03 <fedecf03@gmail.com>
2022-06-04 20:40:03 -0300

    savado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,19 +8,34 @@
                 @foreach ($images as $image)
                     <div class="card">
                         <div class="card-header">
-                            <img src="{{ url('/user/avatar/' . $image->user->image) }}" class="img-thumbnail" width="80px">
-<a href="{{ url('/image/' . $image->id) }}"><span class="nickname">
-                            {{ $image->user->name }}
-                            </span>
-                        </a>
+                            <img src="{{ url('/user/avatar/' . $image->user->image) }}" class="img-thumbnail"
+                                width="80px">
+                            <a href="{{ url('/image/' . $image->id) }}"><span class="nickname">
+                                    {{ $image->user->name }}
+                                </span>
+                            </a>
                         </div>
 
-                        <div class="card-body">
+                        <div class="image-container">
                             <img src="{{ route('getIamge', ['filename' => $image->image_path]) }}" />
 
                         </div>
-                        <a href="" class="btn btn-warning" >comments{{ $image->comments->count() }}</a>
+                    <div>  <h2>comments{{ $image->comments->count() }}</h2>
+                        <hr>
+                        <form method="POST" action="">
+                            @csrf
+                           <input type="hidden" name="image_id" value="{{ $image->id }}" >
+                            <p>
+                               <textarea class="form-control" name="content" required >
 
+                               </textarea>
+                           </p>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-sm btn-primary" value="Add comment">
+                                Submit</button>
+                                </div>
+                        </form>
+                    </div>
                         </a>
                         <div class="like"></div>
                         <div class="desc">{{ $image->description }}</div>
